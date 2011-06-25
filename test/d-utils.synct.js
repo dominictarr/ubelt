8de4f14a4551208263af865416619ca0c68a42fc
Dominic Tarr <dominic.tarr@gmail.com>
2011-06-24 17:27:34 -0700

    automaticially test that all modules have been added

--- test/d-utils.synct.js
+++ test/d-utils.synct.js
@@ -1,19 +1,22 @@
 
 var objects = require('d-utils/objects')
   , arrays = require('d-utils/arrays')
+  , types = require('d-utils/types')
   , u = require('d-utils')
   , it = require('it-is')
-  
-exports ['has all the methods from all the modules'] = function (){
+  , fs = require('fs')
+  , path = require('path')
 
-  it([objects,arrays])
-  .every(function (m){
-  
-    it(u).has(objects.map(m, function (){
+exports ['has all the methods from all the modules'] = function (){
+  var ls = fs.readdirSync(path.join(__dirname, '..'))
+
+  var lsjs = objects.map(objects.filter(ls,/^.*\.js$/),/^(.*)\.js$/)
+
+  objects.mapKeys(lsjs, function (m){
+      var mod = require('d-utils/' + m)
+    return it(u).has(objects.map(mod, function (){
       return it.function()
     }))
-  
+ 
   })
-
-
 }
