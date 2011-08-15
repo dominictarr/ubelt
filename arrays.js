417acf3a3f844bf65916914d911de25189341aaf
Dominic Tarr <dominic.tarr@gmail.com>
2011-08-15 17:36:21 +1000

    stop throwing on empty arrays. bump version 1.0.0

--- arrays.js
+++ arrays.js
@@ -4,6 +4,7 @@ function empty(array){
   return !!!array.length
 }
 
+
 function throwIfEmpty (array, method){
   if(!array.length){
     var err = new Error('arrays.' + method + ' cannot take an empty array')
@@ -12,21 +13,34 @@ function throwIfEmpty (array, method){
   }
 }
 
-function init (array){
+function strictInit (array){
   throwIfEmpty(array)
   return [].slice.call(array, 0, array.length - 1)
 }
-function tail(array){
+function strictTail(array){
   throwIfEmpty(array)
   return [].slice.call(array, 1, array.length )
 }
-function last(array){
+function strictLast(array){
   throwIfEmpty(array)
   return array[array.length -1]  
 }
-function head(array){
+function strictHead(array){
   throwIfEmpty(array)
   return array[0]  
+}
+
+function init (array){
+  return empty(array) ? [] : [].slice.call(array, 0, array.length - 1)
+}
+function tail(array){
+  return empty(array) ? [] : [].slice.call(array, 1, array.length )
+}
+function last(array){
+  return empty(array) ? null : array[array.length - 1]
+}
+function head(array){
+  return empty(array) ? null : array[0]
 }
 
 module.exports = {
@@ -35,5 +49,9 @@ module.exports = {
   tail: tail,
   last: last,
   head: head,
+  strictInit: strictInit,
+  strictTail: strictTail,
+  strictLast: strictLast,
+  strictHead: strictHead,
   empty: empty
 }
