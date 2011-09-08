3d38a8879548069b25a3c8d5bfaba6c7ad012f28
Dominic Tarr <dominic.tarr@gmail.com>
2011-09-08 21:03:55 +1000

    add curry function, bump version 2.3

--- functions.js
+++ functions.js
@@ -1,4 +1,15 @@
-
+var objects = require('./objects')
 exports.id = function (i) {
   return i
+}
+
+exports.curry = function (/*funx, args...*/) {
+  var args = [].slice.call(arguments)
+    , funx = args.shift()
+  return function (){
+    var _args = [].slice.call(arguments)
+    return funx.apply(this, objects.map(args, function (i) {
+      return 'number' == typeof i ? _args[i < 0 ? _args.length + i : i] : i
+    }))
+  }
 }
