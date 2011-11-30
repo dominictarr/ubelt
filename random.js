dc8f36de60e7a22d83334e86c18270574aa9386d
Dominic Tarr <dominic.tarr@gmail.com>
2011-11-30 23:02:19 +1100

    randomOf

--- random.js
+++ random.js
@@ -1,6 +1,10 @@
 
-exports.randomInt = function (min, max) {
+var randomInt = exports.randomInt = function (min, max) {
   min = min || 1
   max = max || 100 
   return Math.round(Math.random()*(max - min)) + min
+}
+
+exports.randomOf = function (array) {
+  return array[randomInt(0, array.length)]
 }
