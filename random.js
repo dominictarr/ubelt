7ca3cf53907c77ffd7e617e72e3e93cf47ceb559
Dominic Tarr <dominic.tarr@gmail.com>
2011-11-30 23:19:34 +1100

    fix dumb error in randomInt

--- random.js
+++ random.js
@@ -1,10 +1,10 @@
 
 var randomInt = exports.randomInt = function (min, max) {
-  min = min || 1
-  max = max || 100 
+  min = min == null ? 1 : min
+  max = max == null ? 100 : max
   return Math.round(Math.random()*(max - min)) + min
 }
 
 exports.randomOf = function (array) {
-  return array[randomInt(0, array.length)]
+  return array[randomInt(0, array.length - 1)]
 }
